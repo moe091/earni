7760d874fba38165a5e82973b77eae54361c9496
moe091 <mnowicki091@gmail.com>
2025-01-31 22:02:28 -0500

    Start setting up data portion of project. Create edgar utility module and implement CIK lookup func

---
+++ data/retrieval/edgar.py
@@ -0,0 +1,34 @@
+"""
+    This module is used to retrieve data from the SEC EDGAR database.
+"""
+
+
+
+def get_cik(ticker):
+    """
+        This method retrieves the CIK number for a given ticker. CIK numbers are used to identify stocks in requests to the EDGAR database.
+
+        (NOTE: For now, the Ticker to CIK data is currently stored in a text file provided by the SEC. Eventually I'll probably migrate it to 
+        a database for slighty faster access, and this function will need to be updated)
+
+        Args: 
+            ticker (str): The ticker symbol of the company.
+
+        Returns:
+            int: the CIK number of the company.
+    """
+    with open('./ticker.txt', 'r', encoding='utf-8') as file:
+        cik = None
+        for line in file:
+            t, c = line.split('\t')
+
+            print(f"Reading line: {t} - {c}")
+            if t.strip() == ticker:
+                cik = c
+                print(f"FOUND TICKER: {t} - {c}")
+                break
+
+        print(f"Returning CIK: {cik}")
+        return cik
+
+            
